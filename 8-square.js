98591f0099e39f11af2af760ffcf4c2030e10119
ofoe <ofoe_98@hotmail.co.uk>
2025-07-14 19:02:41 +0000

    Creation of square

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+const size = Number(process.argv[2]);
+
+if (!Number.isInteger(size)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
