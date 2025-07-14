9327422e2b85693614e0fb7acd1169a1dc1af81f
ofoe <ofoe_98@hotmail.co.uk>
2025-07-14 18:47:34 +0000

    changes made

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const x = Number(process.argv[2]);
 
-if (!Number.isInteger(x) || x < 0) {
+if (!Number.isInteger(x)) {
   console.log("Missing number of occurrences");
 } else {
   for (let i = 0; i < x; i++) {
