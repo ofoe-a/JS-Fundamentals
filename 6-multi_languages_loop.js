d5c7fd05a1419cb293baa6f01ced97170d0cadd4
ofoe <ofoe_98@hotmail.co.uk>
2025-07-14 18:14:26 +0000

    edited for Loops for displaying text

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,13 @@
-const text = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for(let i = 0; i<text.length; i++){
-    console.log(text[i]);
+// const text = ["C is fun", "Python is cool", "JavaScript is amazing"]
+// for(let i = 0; i<text.length; i++){
+//     console.log(text[i]);
+// }
+
+const text = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (let i = 0; i < text.length; i++) {
+  output += text[i] + "\n";
 }
+
+console.log(output.trim());
